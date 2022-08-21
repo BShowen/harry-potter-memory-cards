009be73099416596d3fdff1c7982ecaa432b907d
Bradley Showen <bshowen@live.seminolestate.edu>
2022-08-21 15:46:55 -0400

    Reposition nav within the app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
-import React from "react";
+import React, { useCallback } from "react";
 import Game from "./components/Game";
 import { FiGithub } from "react-icons/fi";
+import useScoreTracker from "./components/useScoreTracker";
+import { ScoreDisplay } from "./components/ScoreDisplay";
 
 export default function App() {
+  const [incrementScore, resetScore, score] = useScoreTracker();
+
+  const updateScore = useCallback(
+    (shouldUpdate) => {
+      if (shouldUpdate) {
+        incrementScore();
+      } else {
+        resetScore();
+      }
+    },
+    [incrementScore, resetScore]
+  );
+
   return (
     <>
+      <nav
+        className="nav sticky-top navbar-dark"
+        style={{ backgroundColor: "#2B3036" }}
+      >
+        <ScoreDisplay {...score} />
+      </nav>
+
       <div id="app-container" className="container bg-dark">
-        <Game />
+        <Game updateScore={updateScore} />
       </div>
 
       <footer className="bg-secondary text-light">
--- src/components/Game.js
+++ src/components/Game.js
@@ -1,42 +1,16 @@
-import React, { useCallback } from "react";
-import useScoreTracker from "./useScoreTracker";
-import { ScoreDisplay } from "./ScoreDisplay";
+import React from "react";
 import { useCards } from "./useCards";
 
-export default function Game() {
-  const [incrementScore, resetScore, score] = useScoreTracker();
-
-  const updateScore = useCallback(
-    (shouldUpdate) => {
-      if (shouldUpdate) {
-        incrementScore();
-      } else {
-        resetScore();
-      }
-    },
-    [incrementScore, resetScore]
-  );
-
+export default function Game({ updateScore }) {
   const [cards] = useCards(updateScore);
 
   return (
-    <>
-      <nav
-        className="nav sticky-top navbar-dark"
-        style={{ backgroundColor: "#2B3036" }}
-      >
-        <ScoreDisplay {...score} />
-      </nav>
-
-      <div className="row justify-content-center pb-3">
-        <div className="row justify-content-center">
-          <div className="col">
-            <div className="d-flex flex-wrap justify-content-center">
-              {cards}
-            </div>
-          </div>
+    <div className="row justify-content-center pb-3">
+      <div className="row justify-content-center">
+        <div className="col">
+          <div className="d-flex flex-wrap justify-content-center">{cards}</div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
